d29fcbeb4f3a42c4188c3f2e92ffb123f537fb00
Oleh Kononenko <olehkon09@gmail.com>
2020-08-05 20:58:11 +0300

    Gallery with hardcode images

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,96 @@ class MyApp extends StatelessWidget {
   }
 }
 
+List heroImageList = [
+  HeroImage(title: 'title1', image: 'assets/images/1.jpg'),
+  HeroImage(title: 'title2', image: 'assets/images/2.jpg'),
+  HeroImage(title: 'title3', image: 'assets/images/3.jpg'),
+  HeroImage(title: 'title4', image: 'assets/images/1.jpg'),
+  HeroImage(title: 'title5', image: 'assets/images/2.jpg'),
+  HeroImage(title: 'title6', image: 'assets/images/3.jpg'),
+  HeroImage(title: 'title7', image: 'assets/images/1.jpg'),
+  HeroImage(title: 'title8', image: 'assets/images/2.jpg'),
+  HeroImage(title: 'title9', image: 'assets/images/3.jpg'),
+  HeroImage(title: 'title10', image: 'assets/images/1.jpg'),
+  HeroImage(title: 'title11', image: 'assets/images/2.jpg'),
+  HeroImage(title: 'title12', image: 'assets/images/3.jpg'),
+
+];
+
+class HeroImage {
+  String title;
+  String image;
+
+  HeroImage({this.title, this.image});
+}
+
+class ImageCard extends StatelessWidget {
+  HeroImage hero;
+  ImageCard({this.hero});
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: hero.title,
+      child: Container(
+        child: Image.asset(hero.image),
+      ),
+    );
+  }
+}
+
+class ImageInfoPage extends StatelessWidget {
+  final HeroImage heroImage;
+
+  ImageInfoPage({this.heroImage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(heroImage.image),
+            Text(heroImage.title)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PhotoList extends StatefulWidget {
+  @override
+  _PhotoListState createState() => _PhotoListState();
+}
+
+class _PhotoListState extends State<PhotoList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverGrid(
+              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 300,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10),
+              delegate: SliverChildBuilderDelegate((context, index) {
+                return GestureDetector(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  ImageCard(hero:heroImageList[index],)),
+                    ), child: ImageCard(hero: heroImageList[index]),);
+              }, childCount: heroImageList.length),
+            )
+          ],
+        ));
+  }
+}
+
 class ColorSwitcher extends StatefulWidget {
   ColorSwitcher({Key key}) : super(key: key);
   @override
@@ -43,6 +133,25 @@ class _ColorSwitcherState extends State<ColorSwitcher> {
     setState(() => backgroundColor = getRandomColor());
   }
 
+  Route _createRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => PhotoList(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(0.0, 1.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
@@ -50,7 +159,10 @@ class _ColorSwitcherState extends State<ColorSwitcher> {
           statusBarColor: Colors.transparent,
         ),
         child: Scaffold(
-          floatingActionButton: FloatingActionButton(child: Icon(Icons.add),),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () => {Navigator.of(context).push(_createRoute())},
+              child: Icon(Icons.image),
+            ),
             body: InkWell(
               child: Container(
                 width: MediaQuery.of(context).size.width,
@@ -60,17 +172,10 @@ class _ColorSwitcherState extends State<ColorSwitcher> {
                   children: <Widget>[
                     Text('Hey there',
                         style: Theme.of(context).textTheme.headline2),
-                    //Positioned(
-                      //  left: MediaQuery.of(context).size.width,
-                        //bottom: 0,
-                        //child: Container(
-                      //child: Text(backgroundColor.toString()),
-                    //))
                   ],
                 ),
               ),
               onTap: () => changeColor(),
-              //splashColor: backgroundColor,
             )));
   }
 }
